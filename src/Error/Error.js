2ac588f7e616cc691c33ca68893247ac3b553374
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-17 13:24:26 -0500

    Apply styling to loading

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -5,7 +5,7 @@ import './Error.css';
 const Error = () => {
   return(
     <>
-    <img alt="spilling coffee" src="https://media.giphy.com/media/GhNCif5GnFBYY/giphy.gif"/>
+    <img className='error' alt="spilling coffee" src="https://media.giphy.com/media/GhNCif5GnFBYY/giphy.gif"/>
     <h2 className='msg'>There's been an error!</h2>
     </>
   )
--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
-import './Loading.css'
-
 const Loading = () => {
 
   return (
-    <>
-      <img alt='pouring coffee' src='https://media.giphy.com/media/xUOwG7uIuaZVDg86yc/giphy.gif' />
       <h2 className='msg'>Please wait...</h2>
-    </>
   )
 }
 
