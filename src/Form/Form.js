69a3a5c17f458895af181a2a517cc17efa15047a
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-17 12:02:47 -0500

    Create state so on change, users see the now updated state

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,10 +3,57 @@ import React, { Component } from 'react';
 import './Form.css';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: Date.now(),
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+
+//number is a number data type, must change from string to num ParseInt()
 
   render() {
     return (
-      <p>form here</p>
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Number of Guests'
+          name='number'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+        />
+        <button className='reserve'>Make Reservation</button>
+      </form>
     )
   }
 }
